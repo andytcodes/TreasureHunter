d33a12b3d69a89ebe9e4f7cbd8474955e0433329
Andy <andytcodes@gmail.com>
2021-02-17 01:22:20 -0500

    Added cube rotation, arrow image, design folder

--- TreasureHunter/Assets/Scripts/Movement.cs
+++ TreasureHunter/Assets/Scripts/Movement.cs
@@ -9,6 +9,7 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //WASD Movement
         if (Input.GetKey(KeyCode.W)) 
         {
             this.transform.Translate(0, 0, m_speed * Time.deltaTime);
@@ -24,6 +25,16 @@ public class Movement : MonoBehaviour
         else if (Input.GetKey(KeyCode.A))
         {
             this.transform.Translate(-m_speed * Time.deltaTime, 0, 0);
+        }
+
+        //Rotation
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            this.transform.Rotate(0, -40, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            this.transform.Rotate(0, 40, 0);
         }
     }
 }
