bb5fd922c7229c988af342a8d32321248cff4a48
SarahRadulovich-school <radulovs@sheridancollege.ca>
2021-02-17 14:24:41 -0500

    Added new assets for terrain, finished the main menu scene, camera has movement.

---
+++ TreasureHunter/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ExitButton : MonoBehaviour
+{
+    public void ExitPlayMode()
+    {
+        EditorApplication.isPlaying = false;
+    }
+}
