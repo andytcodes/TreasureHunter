3fd1261bb5f3f3436121177850f93dc920bb8ad3
Andy <andytcodes@gmail.com>
2021-02-18 01:31:01 -0500

    camera glued on terrain by 4 to prevent terrain collision

--- TreasureHunter/Assets/Scripts/TerrainMovement.cs
+++ TreasureHunter/Assets/Scripts/TerrainMovement.cs
@@ -9,9 +9,23 @@ public class TerrainMovement : MonoBehaviour
     void Update()
     {
         GameObject cube = GameObject.Find("Cube");
+        GameObject sphere = GameObject.Find("TreasureSphere");
+        GameObject cam = GameObject.Find("Cam");
+
         Vector3 cubePos;
+        Vector3 spherePos;
+        Vector3 camPos;
+
         cubePos = cube.transform.position;
+        spherePos = sphere.transform.position;
+        camPos = cam.transform.position;
+
         cubePos.y = Terrain.activeTerrain.SampleHeight(cube.transform.position) + 1;
+        spherePos.y = Terrain.activeTerrain.SampleHeight(sphere.transform.position) + 1;
+        camPos.y = Terrain.activeTerrain.SampleHeight(cam.transform.position) + 4;
+
         cube.transform.position = cubePos;
+        sphere.transform.position = spherePos;
+        cam.transform.position = camPos;
     }
 }
