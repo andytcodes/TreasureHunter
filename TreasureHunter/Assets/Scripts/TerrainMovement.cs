975707cf5d94f7ae74b71a293e45924b9bb4c727
Andy <andytcodes@gmail.com>
2021-02-18 00:56:16 -0500

    cuber glued on terrain added

---
+++ TreasureHunter/Assets/Scripts/TerrainMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainMovement : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject cube = GameObject.Find("Cube");
+        Vector3 cubePos;
+        cubePos = cube.transform.position;
+        cubePos.y = Terrain.activeTerrain.SampleHeight(cube.transform.position) + 1;
+        cube.transform.position = cubePos;
+    }
+}
